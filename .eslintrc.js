9c9dbc9468dbf177bb9ed217f82601ef1c6f440f
linkerGitHub <linker-junhao@outlook.com>
2020-05-24 22:48:57 +0800

    调整eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     "vue/no-deprecated-scope-attribute": "error",
-    "vue/no-deprecated-slot-attribute": "error",
+    "vue/no-deprecated-slot-attribute": "off",
     "vue/no-deprecated-slot-scope-attribute": "error",
     "vue/no-irregular-whitespace": "error",
     "vue/no-reserved-component-names": "error",
@@ -26,6 +26,6 @@ module.exports = {
       2
     ],
     "quotes": ["error", "single"],
-    "compat/compat":"warn"
+    "compat/compat":"off"
   },
 };
