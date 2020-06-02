43d0fd1625c9c18295a70dee2638e06a45e8193d
linkerGitHub <linker-junhao@outlook.com>
2020-06-02 13:55:30 +0800

    优化骨架并补充manageTable

--- src/main.js
+++ src/main.js
@@ -4,10 +4,17 @@ import route from './route'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import Axios from 'axios'
+import config from '@/base-lib/utils/axios/config'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 Vue.use(VueRouter)
+
+Axios.defaults = {
+  ...Axios.defaults,
+  ...config
+}
 
 new Vue({
   router: route,
--- src/route/index.js
+++ src/route/index.js
@@ -8,25 +8,62 @@ const routes = [
   {
     path: '/index',
     component: () => import('@/views/Index.vue'),
+    redirect: '/index/page',
     children: [
       {
-        path: 'test',
-        component: () => import('@/views/test.vue'),
+        path: 'page',
+        name: 'Page',
+        component: () => import('@/views/singlePage/Index.vue'),
         meta: {
-          title: 'test'
+          title: '单页'
+        }
+      },
+      {
+        path: 'article',
+        name: 'Article',
+        component: () => import('@/views/article/Index.vue'),
+        meta: {
+          title: '文章'
+        }
+      },
+      {
+        path: 'category',
+        name: 'Category',
+        component: () => import('@/views/category/Index.vue'),
+        meta: {
+          title: '类别'
+        }
+      },
+      {
+        path: 'team',
+        name: 'Team',
+        component: () => import('@/views/team/Index.vue'),
+        meta: {
+          title: '专家团队'
         }
       },
       {
         path: 'sys-config',
+        name: 'SysConfig',
         component: () => import('@/views/sysConfig/Index'),
         meta: {
           title: '系统配置'
         }
       }
     ]
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login/Index')
   }
 ]
 
-export default new VueRouter({
+const router = new VueRouter({
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  next()
+})
+
+export default router
