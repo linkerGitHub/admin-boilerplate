94ace0fe631555f1f809cab7780a359e4437e508
linker <373482670@qq.com>
2020-06-04 17:41:41 +0800

    完善后台框架

--- src/base-lib/components/uploader/uploader.vue
+++ src/base-lib/components/uploader/uploader.vue
@@ -1,15 +1,41 @@
 <template>
-  <el-upload
-    ref="uploader"
-    :action="uploadUrl"
-    name="pic"
-    :file-list="fileList"
-    list-type="picture-card"
-    :auto-upload="false"
-    :on-success="onSuccess"
-  >
-    <i class="el-icon-plus" />
-  </el-upload>
+  <div>
+    <el-upload
+      v-if="limit !== 1"
+      ref="uploader"
+      :action="uploadUrl"
+      name="pic"
+      :file-list="fileList"
+      list-type="picture-card"
+      :auto-upload="false"
+      :on-success="onSuccess"
+      :on-change="uploaderFileChangeHandle"
+      :limit="limit"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-upload
+      ref="uploader"
+      :action="uploadUrl"
+      name="pic"
+      :show-file-list="false"
+      :auto-upload="false"
+      :on-success="onSuccess"
+      :on-change="uploaderFileChangeHandle"
+      :limit="limit"
+    >
+      <img
+        v-if="files.length > 0"
+        :src="files[0].url"
+        class="avatar cov-preview"
+        alt="preview"
+      >
+      <i
+        v-else
+        class="el-icon-plus avatar-uploader-icon"
+      />
+    </el-upload>
+  </div>
 </template>
 
 <script>
@@ -17,10 +43,22 @@
 export default {
   name: 'Uploader',
   props: {
+    fileListOfUploader: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
     disabled: {
       type: Boolean,
       default() {
         return false
+      }
+    },
+    limit: {
+      type: Number,
+      default() {
+        return Infinity
       }
     },
     uploadUrl: {
@@ -40,7 +78,25 @@ export default {
   },
   data() {
     return {
+      files: this.fileList
     }
+  },
+  watch: {
+    fileList: function (val, oldVal) {
+      const ret = val.filter(item => {
+        return oldVal.findIndex(it => {
+          return it.url === item.url
+        }) === -1
+      })
+      if(ret.length > 0) {
+        this.files = val
+      }
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$emit('update:fileListOfUploader', this.fileList)
+    })
   },
   methods: {
     submit() {
@@ -48,11 +104,41 @@ export default {
     },
     onSuccess(res, file, fileList) {
       this.onSuccessCall(res, file, fileList)
+    },
+    uploaderFileChangeHandle(file, fileList) {
+      if(this.limit === 1) {
+        if (file.status === 'ready') {
+          const fileR = new FileReader()
+          fileR.onload = (e) => {
+            this.$set(file, 'url', e.target.result)
+          }
+          fileR.readAsDataURL(file.raw)
+        }
+        this.$emit('update:fileListOfUploader', [file])
+        this.$emit('change', file, [file])
+        this.files = [file]
+      } else {
+        this.$emit('update:fileListOfUploader', fileList)
+        this.$emit('change', file, fileList)
+        this.files = fileList
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .cov-preview{
+    height: 120px;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 150px;
+    height: 120px;
+    line-height: 120px !important;
+    text-align: center;
+    border: 1px dashed #DCDFE6;
+    border-radius: 4px;
+  }
 </style>
