c7af5fda70e805a83912c1da4fc1f3b450e0467e
linkerGitHub <linker-junhao@outlook.com>
2020-06-11 00:48:17 +0800

    添加图集管理

---
+++ src/views/collection/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <manage-table
+      :columns-definition="def"
+      :data-src-url="'/collection'"
+    />
+  </div>
+</template>
+
+<script>
+import ManageTable from '@/base-lib/components/ManageTable/Index.vue'
+export default {
+  name: 'Index',
+  components: {ManageTable},
+  data() {
+    return {
+      def: [
+        {
+          prop: 'collection_name',
+          label: '图集名称',
+          editable: true
+        },
+        {
+          prop: 'collection_description',
+          label: '图集描述',
+          editable: true
+        },
+        {
+          prop: 'pic_count',
+          label: '图片数量'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
